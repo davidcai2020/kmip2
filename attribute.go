dfc9ed5ccdf26df6d102666551e6c6179077f1e0
davidcai2020 <david_cai@msn.com>
2020-10-02 17:29:53 -0700

    create kmip2

---
+++ attribute.go
@@ -0,0 +1,72 @@
+package kmip
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import (
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+// Attribute is a Attribute Object Structure
+type Attribute struct {
+	Tag `kmip:"ATTRIBUTE"`
+
+	Name  string      `kmip:"ATTRIBUTE_NAME"`
+	Index int32       `kmip:"ATTRIBUTE_INDEX"`
+	Value interface{} `kmip:"ATTRIBUTE_VALUE"`
+}
+
+// BuildFieldValue builds dynamic Value field
+func (a *Attribute) BuildFieldValue(name string) (v interface{}, err error) {
+	switch a.Name {
+	case "Cryptographic Algorithm":
+		v = Enum(0)
+	case "Cryptographic Length", "Cryptographic Usage Mask":
+		v = int32(0)
+	case "Unique Identifier":
+		v = ""
+	case "Object Type":
+		v = Enum(0)
+	case "Initial Date", "Last Change Date":
+		v = time.Time{}
+	case "Name":
+		v = &Name{}
+	default:
+		err = errors.Errorf("unsupported attribute: %v", a.Name)
+	}
+
+	return
+}
+
+// Attributes is a sequence of Attribute objects which allows building and search
+type Attributes []Attribute
+
+func (attrs Attributes) Get(name string) (val interface{}) {
+	for i := range attrs {
+		if attrs[i].Name == name {
+			val = attrs[i].Value
+			break
+		}
+	}
+
+	return
+}
+
+// TemplateAttribute is a Template-Attribute Object Structure
+type TemplateAttribute struct {
+	Tag `kmip:"TEMPLATE_ATTRIBUTE"`
+
+	Name       Name       `kmip:"NAME"`
+	Attributes Attributes `kmip:"ATTRIBUTE"`
+}
+
+// Name is a Name Attribute Structure
+type Name struct {
+	Tag `kmip:"NAME"`
+
+	Value string `kmip:"NAME_VALUE,required"`
+	Type  Enum   `kmip:"NAME_TYPE,required"`
+}
---
+++ operations.go
@@ -0,0 +1,78 @@
+package kmip
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// CreateRequest is a Create Request Payload
+type CreateRequest struct {
+	ObjectType        Enum              `kmip:"OBJECT_TYPE,required"`
+	TemplateAttribute TemplateAttribute `kmip:"TEMPLATE_ATTRIBUTE,required"`
+}
+
+// CreateResponse is a Create Response Payload
+type CreateResponse struct {
+	ObjectType        Enum              `kmip:"OBJECT_TYPE,required"`
+	UniqueIdentifier  string            `kmip:"UNIQUE_IDENTIFIER,required"`
+	TemplateAttribute TemplateAttribute `kmip:"TEMPLATE_ATTRIBUTE"`
+}
+
+// GetRequest is a Get Request Payload
+type GetRequest struct {
+	UniqueIdentifier   string                   `kmip:"UNIQUE_IDENTIFIER"`
+	KeyFormatType      Enum                     `kmip:"KEY_FORMAT_TYPE"`
+	KeyWrapType        Enum                     `kmip:"KEY_WRAP_TYPE"`
+	KeyCompressionType Enum                     `kmip:"KEY_COMPRESSION_TYPE"`
+	KeyWrappingSpec    KeyWrappingSpecification `kmip:"KEY_WRAPPING_SPECIFICATION"`
+}
+
+// GetResponse is a Get Response Payload
+type GetResponse struct {
+	ObjectType       Enum   `kmip:"OBJECT_TYPE,required"`
+	UniqueIdentifier string `kmip:"UNIQUE_IDENTIFIER,required"`
+	// Response might contain one of SymmetricKey, Certificate, ...
+	SymmetricKey SymmetricKey `kmip:"SYMMETRIC_KEY"`
+}
+
+// GetAttributesRequest is a Get Attributes Request Payload
+type GetAttributesRequest struct {
+	UniqueIdentifier string   `kmip:"UNIQUE_IDENTIFIER"`
+	AttributeNames   []string `kmip:"ATTRIBUTE_NAME"`
+}
+
+// GetAttributesResponse is a Get Attributes Response Payload
+type GetAttributesResponse struct {
+	UniqueIdentifier string     `kmip:"UNIQUE_IDENTIFIER,required"`
+	Attributes       Attributes `kmip:"ATTRIBUTE"`
+}
+
+// GetAttributeListRequest is a Get Attribute List Request Payload
+type GetAttributeListRequest struct {
+	UniqueIdentifier string `kmip:"UNIQUE_IDENTIFIER"`
+}
+
+// GetAttributeListResponse is a Get Attribute List Response Payload
+type GetAttributeListResponse struct {
+	UniqueIdentifier string   `kmip:"UNIQUE_IDENTIFIER,required"`
+	AttributeNames   []string `kmip:"ATTRIBUTE_NAME"`
+}
+
+// DestroyRequest is a Destroy Request Payload
+type DestroyRequest struct {
+	UniqueIdentifier string `kmip:"UNIQUE_IDENTIFIER"`
+}
+
+// DestroyResponse is a Destroy Response Payload
+type DestroyResponse struct {
+	UniqueIdentifier string `kmip:"UNIQUE_IDENTIFIER,required"`
+}
+
+// DiscoverVersionsRequest is a Discover Versions Request Payload
+type DiscoverVersionsRequest struct {
+	ProtocolVersions []ProtocolVersion `kmip:"PROTOCOL_VERSION"`
+}
+
+// DiscoverVersionsResponse is a Discover Versions Response Payload
+type DiscoverVersionsResponse struct {
+	ProtocolVersions []ProtocolVersion `kmip:"PROTOCOL_VERSION"`
+}
